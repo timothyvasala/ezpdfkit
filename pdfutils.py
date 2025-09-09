38d152930ec8b1518736e00b4f2dcf580685f92d
timothyvasala <timothyvasala@gmail.com>
2025-09-09 23:24:16 +0530

    old code merge request before proceeding to new labs code

--- pdfutils.py
+++ pdfutils.py
@@ -23,3 +23,47 @@ def split_pdf(uploaded_file, page_numbers):
     output = io.BytesIO()
     writer.write(output)
     return output.getvalue()
+
+# Add these new functions to your pdfutils.py file:
+
+def compress_pdf(uploaded_file, compression_level='medium'):
+    """Compress PDF by reducing image quality and removing metadata."""
+    reader = PdfReader(uploaded_file)
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        page.compress_content_streams()  # Basic compression
+        writer.add_page(page)
+
+    writer.add_metadata({})  # Remove metadata for smaller size
+
+    output = io.BytesIO()
+    writer.write(output)
+    return output.getvalue()
+
+def rotate_pdf(uploaded_file, rotation_angle):
+    """Rotate all pages in PDF by specified angle."""
+    reader = PdfReader(uploaded_file)
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        rotated_page = page.rotate(rotation_angle)
+        writer.add_page(rotated_page)
+
+    output = io.BytesIO()
+    writer.write(output)
+    return output.getvalue()
+
+def protect_pdf(uploaded_file, password):
+    """Add password protection to PDF."""
+    reader = PdfReader(uploaded_file)
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        writer.add_page(page)
+
+    writer.encrypt(password)
+
+    output = io.BytesIO()
+    writer.write(output)
+    return output.getvalue()
